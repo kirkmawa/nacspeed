1eabd993825f1d7e4775bda135ec43c913fd24e2
Drew Kirkman <dkirkman@outlook.com>
2015-10-21 12:44:01 -0400

    implement method to find the newest log file. This will be used at startup and when the software determines the log has been rotated

--- nacspeed.py
+++ nacspeed.py
@@ -20,11 +20,13 @@ def modify_time (filename):
 	t = os.path.getmtime(filename)
 	return datetime.datetime.fromtimestamp(t)
 
-d = datetime.datetime.now()
-for n in range(1,20):
-	logfilename = Config['nacspeed']['nsroot'] + "NetSight/appdata/logs/" + d.strftime("nacESE.%Y_%m_%d_") + n.zfill(2) ".log"
-	print("Checking " + logfilename + "...")
-	if os.path.exists(logfilename):
-		initlog = logfilename
-print ("Using " + initlog + " for initial logfile")
+def find_logfile():
+	d = datetime.datetime.now()
+	print ("Finding latest logfile...")
+	for n in range(1,20):
+		logfilename = Config['nacspeed']['nsroot'] + "NetSight/appdata/logs/" + d.strftime("nacESE.%Y_%m_%d_") + format(n, '02d') + ".log"
+		if os.path.exists(logfilename):
+			initlog = logfilename
+	print ("Using" + initlog)
+	return initlog
 
